291c013dfe2533d91593c7bed3892a113e76e387
hgueguen <hortense.gueguen@etudiant.univ-lr.fr>
2024-09-09 18:38:10 +1000

    All the pages are ready, the navbar is almost finish

--- entertainment-guild/src/App.js
+++ entertainment-guild/src/App.js
@@ -3,16 +3,21 @@ import React from 'react';
 import {Routes, Route} from "react-router-dom";
 import Home from "./pages/Home.js";
 import Login from "./pages/Login";
-import NavBar from "./component/NavBar";
 import Books from "./pages/Books";
 import Games from "./pages/Games";
 import Movies from "./pages/Movies";
 import Cart from "./pages/Cart";
+import ErrorNotFound from "./pages/ErrorNotFound";
+import SignUp from "./pages/SignUp";
+import Management from "./pages/Management";
+import ManageUsers from "./pages/ManageUsers";
+import ManageProducts from "./pages/ManageProducts";
+import NavBarUsers from "./component/NavBarUsers";
 
 const App = () => {
   return (
     <>
-      <NavBar />
+      <NavBarUsers />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -20,7 +25,11 @@ const App = () => {
         <Route path="/games" element={<Games />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="*" element={<h1>Not Found</h1>} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/management" element={<Management />} />
+        <Route path="/manageusers" element={<ManageUsers />} />
+        <Route path="/manageproducts" element={<ManageProducts />} />
+        <Route path="*" element={<ErrorNotFound />} />
       </Routes>
     </>
   );

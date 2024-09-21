b0e8b0ccfce437cd7d406b575fe5f395f13f3dd7
hgueguen <hortense.gueguen@etudiant.univ-lr.fr>
2024-09-21 15:20:22 +1000

    Movie page finish

--- entertainment-guild/src/pages/Movies.js
+++ entertainment-guild/src/pages/Movies.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {TextField} from "@mui/material";
+import ProductList from "../component/ProductList";
 
 const Movies = () => {
+
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
+    
     return (
         <div>
             <h1>Movies</h1>
+            <div className="containerTextField">
+                <TextField
+                    id="standard-basic"
+                    label="Search books..."
+                    variant="standard"
+                    value={searchTerm}
+                    onChange={handleSearch}
+                    className="searchField"
+                />
+            </div>
+            <ProductList
+                apiUrl='http://localhost:8080/api/v1/db/data/v1/inft3050/Product?limit=1000'
+                genre={2}
+                searchTerm={searchTerm}
+            />
         </div>
     );
 }
